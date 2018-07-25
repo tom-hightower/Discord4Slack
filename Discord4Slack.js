f77371da898a61bd0e67f81d20929475310cad34
Tom Hightower <tom.hightower@asurion.com>
2018-07-25 10:43:41 -0500

    Fixed 'all threads' coloring

--- Discord4Slack.js
+++ Discord4Slack.js
@@ -9,13 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
 	ts-message:hover, ts-thread .collapse_inline_thread_container:hover, ts-thread .view_all_replies_container:hover { background: #2C2F33 !important; }
 	.c-search_modal .popover>div, .c-search__input_box, .c-search_autocomplete footer, .c-dialog__content, .c-dialog__header, .c-dialog__footer { background: #23272A; color: #FFF; }
 	.c-search__input_and_close { border-bottom: 1px solid #7289da !important; }
-	.c-search, .p-search_filter__title_text { color: #FFF; background: #23272A !important; }
+	.c-search, .p-search_filter__title_text, .p-file_upload_dialog__preview { color: #FFF; background: #23272A !important; }
 	ts-message, ts-thread .thread_messages { background: #23272A; }
 	.p-flexpane_header, #flex_contents .heading { background: #23272A; border-bottom: 1px solid #7289da; }
 	.c-dialog__footer .p-file_upload_dialog__footer_share_inputs, .c-dialog__title, #flex_contents .heading_text { color: #FFF; }
 	.p-downloads_list__shift_hint { background: linear-gradient(180deg,hsla(0,0%,100%,0),#23272A 25%,#23272A); }
 	.p-download_item__container .p-download_item__name_row { color: #FFF; }
-	#threads_msgs_scroller_div .threads_caught_up_divider .divider_label { background: #7289DA; }
+	.pinned_item { border: 1px solid #7289da; }
+	#threads_msgs .inline_message_input_container.with_file_upload .inline_file_upload:hover .upload_plus_icon { color: #7289da !important; }
+	#threads_msgs .inline_message_input_container.with_file_upload .inline_file_upload, .reply_input_container .inline_message_input_container .ql-container.focus, #threads_msgs .inline_message_input_container.with_file_upload, .reply_input_container .inline_message_input_container .ql-container { background: #545454 !important; border-color: #545454 !important; }
+	#threads_msgs_scroller_div .threads_caught_up_divider .divider_label, .ts_toggle.checked .ts_toggle_button, { background: #7289DA !important; }
 	#threads_msgs_scroller_div .threads_caught_up_divider .divider_line { border-top: 1px solid #7289da; }
     `;
     $.ajax({
