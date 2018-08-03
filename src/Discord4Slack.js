0c1cc98f3e908d6e667b066a7a73054224f7f367
Tom Hightower <tom.hightower@asurion.com>
2018-08-03 04:50:42 -0500

    Cleaned up slightly

--- src/Discord4Slack.js
+++ src/Discord4Slack.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	.c-search_autocomplete__suggestion_content .c-search_autocomplete__suggestion_text .token, .c-search_autocomplete__suggestion_content, .ql-container.texty_single_line_input { color: var(--fullWhite) !important; }
 	.c-filter_input, .c-input_select_options_list_container { color: var(--fullWhite); background-color: var(--trueGrey) !important; border: 1px solid var(--blurple) !important; }
 	ts-message:hover, ts-thread .collapse_inline_thread_container:hover, ts-thread .view_all_replies_container:hover { background: var(--notBlack) !important; }
-	.c-search_modal .popover>div, .c-search__input_box, .c-search_autocomplete footer, .c-dialog__content, .c-dialog__header, .c-dialog__footer { background: var(--notQuiteBlack); color: var(--fullWhite); }
     `;
     $.ajax({
         url: 'https://raw.githubusercontent.com/tom-hightower/Discord4Slack/master/src/discordTheme.css',
